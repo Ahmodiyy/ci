fea05998eb05901fff8fc2d774ec9d7bbdc14585
Ahmodiyy <ahmodolaitan03@gmail.com>
2025-04-29 09:56:01 +0100

    first ci with jenkins

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest{
+    @Test
+    void appTest(){
+        App app = new App();
+        String appName = app.app();
+        Assertions.assertEquals("ci", appName);
+    }
+}
